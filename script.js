66eadbf2765840e187fe113150dec30803d7e182
Abhijit Parad <ErzaXAbhi>
2023-07-20 16:44:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let text = document.getElementById("text");
+let leaf = document.getElementById("leaf");
+let hill1 = document.getElementById("hill1");
+let hill4 = document.getElementById("hill4");
+let hill5 = document.getElementById("hill5");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+
+  text.style.marginTop = value * 2.5 + "px";
+  leaf.style.top = value * -1.5 + "px";
+  leaf.style.left = value * 1.5 + "px";
+  hill5.style.left = value * 1.5 + "px";
+  hill4.style.left = value * -1.5 + "px";
+  hill1.style.top = value * 1 + "px";
+});
+
+// Flashing Title
+
+let alertShow = false;
+
+setInterval(() => {
+  document.title = alertShow
+    ? "Parallax Scrolling Website"
+    : "By Abhijit Parad";
+
+  alertShow = !alertShow;
+}, 1000);
